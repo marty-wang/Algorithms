d3319991533c35c33821021de1042ea37d893263
Mo Wang <mo.hy.wang@gmail.com>
2011-10-14 23:59:39 -0700

    first commit

---
+++ js/linkedlist.js
@@ -0,0 +1,64 @@
+(function() {
+  var $, Node, Stack, StackIterator, _ref;
+  $ = (_ref = window.Alg) != null ? _ref : window.Alg = {};
+  Node = (function() {
+    function Node(item) {
+      this.item = item != null ? item : null;
+      this.next = null;
+    }
+    return Node;
+  })();
+  Stack = (function() {
+    function Stack() {
+      this._first = null;
+      this._count = 0;
+    }
+    Stack.prototype.push = function(item) {
+      var newFirst;
+      if (item == null) {
+        throw "item cannot be undefined nor null";
+      }
+      newFirst = new Node(item);
+      newFirst.next = this._first;
+      this._first = newFirst;
+      return ++this._count;
+    };
+    Stack.prototype.pop = function() {
+      var item;
+      if (this.isEmpty()) {
+        throw "stack is empty";
+      }
+      item = this._first.item;
+      this._first = this._first.next;
+      this._count--;
+      return item;
+    };
+    Stack.prototype.isEmpty = function() {
+      return this._count <= 0;
+    };
+    Stack.prototype.size = function() {
+      return this._count;
+    };
+    Stack.prototype.iterator = function() {
+      return new StackIterator(this);
+    };
+    return Stack;
+  })();
+  StackIterator = (function() {
+    function StackIterator(stack) {
+      this._stack = stack;
+      this._current = stack._first;
+    }
+    StackIterator.prototype.hasNext = function() {
+      return this._current != null;
+    };
+    StackIterator.prototype.next = function() {
+      var oldCurrent;
+      oldCurrent = this._current;
+      this._current = this._current.next;
+      return oldCurrent.item;
+    };
+    return StackIterator;
+  })();
+  $.Stack = Stack;
+}).call(this);
