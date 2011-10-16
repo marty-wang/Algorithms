634114acc59d2a01dba125f7d4249365aa56c15e
Mo Wang <mo.hy.wang@gmail.com>
2011-10-16 11:41:54 -0700

    add stack demo

--- app/coffee/app.coffee
+++ app/coffee/app.coffee
@@ -1,19 +1,61 @@
 App ?= {}
 
+###############################################################################  
+
 do (App) ->
   class StackDemo
   
-    constructor: (container, width = 640, height = 480) ->
-      @_paper = Raphael container, width, height
+    constructor: (container, width = 640, height = 480, initPosition = 400) ->
       @_width = width
       @_height = height
+      @_data = new Alg.Stack()
+      @_paper = Raphael container, width, height
+      @_countText = null
+      @_lastY = initPosition
 
-      _init.call this
+      @_duration = 400
+      @_itemInitX = 100
+      @_itemInitY = -32
+      @_stepDistance = 34
+
+      _setup.call this
+    
+    push: (item) =>
+      c = @_paper.rect @_itemInitX, @_itemInitY, 120, 30
+      @_data.push c
+      c.attr({
+        fill: 'black',
+        stroke: 'white'
+      }).animate({
+        y: @_lastY
+      }, @_duration, '<>')
+      @_lastY -= @_stepDistance
+      @_countText.attr "text", @_data.size()
+
+    pop: ->
+      try
+        c = @_data.pop()
+        c.animate {
+          y: @_itemInitY
+        }, @_duration, '>'
+        @_lastY += @_stepDistance
+        @_countText.attr "text", @_data.size()
+      catch error
+        alert "#{error}"
 
     # Private
 
-    _init = ->
+    _setup = ->
       @_paper.rect(0, 0, @_width, @_height, 10).attr({fill: "gray", stroke: "none"});
+      ct = @_paper.text 420, 250, "0";
+      ct.attr {
+        fill: "white"
+        "font-family": "Arial"
+        "font-weight": 800
+        "font-size": 200
+      }
+      @_countText = ct
+
       
   App.StackDemo = StackDemo
   
@@ -36,4 +78,15 @@ do (App) ->
 
 $ ->
   stackDemo = new App.StackDemo "stack"
+  
+  $stackAddButton = $ '#stack .add'
+  $stackAddButton.click (e)->
+    e.preventDefault()
+    stackDemo.push "foo"
+
+  $stackRemoveButton = $ '#stack .remove'
+  $stackRemoveButton.click (e)->
+    e.preventDefault()
+    stackDemo.pop()
+
   queueDemo = new App.QueueDemo "queue"
--- app/compiled/app.js
+++ app/compiled/app.js
@@ -1,28 +1,75 @@
 (function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   if (typeof App === "undefined" || App === null) {
     App = {};
   }
   (function(App) {
     var StackDemo;
     StackDemo = (function() {
-      var _init;
-      function StackDemo(container, width, height) {
+      var _setup;
+      function StackDemo(container, width, height, initPosition) {
         if (width == null) {
           width = 640;
         }
         if (height == null) {
           height = 480;
         }
-        this._paper = Raphael(container, width, height);
+        if (initPosition == null) {
+          initPosition = 400;
+        }
+        this.push = __bind(this.push, this);
         this._width = width;
         this._height = height;
-        _init.call(this);
+        this._data = new Alg.Stack();
+        this._paper = Raphael(container, width, height);
+        this._countText = null;
+        this._lastY = initPosition;
+        this._duration = 400;
+        this._itemInitX = 100;
+        this._itemInitY = -32;
+        this._stepDistance = 34;
+        _setup.call(this);
       }
-      _init = function() {
-        return this._paper.rect(0, 0, this._width, this._height, 10).attr({
+      StackDemo.prototype.push = function(item) {
+        var c;
+        c = this._paper.rect(this._itemInitX, this._itemInitY, 120, 30);
+        this._data.push(c);
+        c.attr({
+          fill: 'black',
+          stroke: 'white'
+        }).animate({
+          y: this._lastY
+        }, this._duration, '<>');
+        this._lastY -= this._stepDistance;
+        return this._countText.attr("text", this._data.size());
+      };
+      StackDemo.prototype.pop = function() {
+        var c;
+        try {
+          c = this._data.pop();
+          c.animate({
+            y: this._itemInitY
+          }, this._duration, '>');
+          this._lastY += this._stepDistance;
+          return this._countText.attr("text", this._data.size());
+        } catch (error) {
+          return alert("" + error);
+        }
+      };
+      _setup = function() {
+        var ct;
+        this._paper.rect(0, 0, this._width, this._height, 10).attr({
           fill: "gray",
           stroke: "none"
         });
+        ct = this._paper.text(420, 250, "0");
+        ct.attr({
+          fill: "white",
+          "font-family": "Arial",
+          "font-weight": 800,
+          "font-size": 200
+        });
+        return this._countText = ct;
       };
       return StackDemo;
     })();
@@ -43,8 +90,18 @@
     return App.QueueDemo = QueueDemo;
   })(App);
   $(function() {
-    var queueDemo, stackDemo;
+    var $stackAddButton, $stackRemoveButton, queueDemo, stackDemo;
     stackDemo = new App.StackDemo("stack");
+    $stackAddButton = $('#stack .add');
+    $stackAddButton.click(function(e) {
+      e.preventDefault();
+      return stackDemo.push("foo");
+    });
+    $stackRemoveButton = $('#stack .remove');
+    $stackRemoveButton.click(function(e) {
+      e.preventDefault();
+      return stackDemo.pop();
+    });
     return queueDemo = new App.QueueDemo("queue");
   });
 }).call(this);
