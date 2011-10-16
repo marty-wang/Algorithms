9745057a95cf90a208ef8e436178bd72359fcb3d
Mo Wang <mo.hy.wang@gmail.com>
2011-10-15 22:36:39 -0700

    add demo

--- app/coffee/app.coffee
+++ app/coffee/app.coffee
@@ -1,1 +1,39 @@
+App ?= {}
+
+do (App) ->
+  class StackDemo
+  
+    constructor: (container, width = 640, height = 480) ->
+      @_paper = Raphael container, width, height
+      @_width = width
+      @_height = height
+
+      _init.call this
+
+    # Private
+
+    _init = ->
+      @_paper.rect(0, 0, @_width, @_height, 10).attr({fill: "gray", stroke: "none"});
+      
+  App.StackDemo = StackDemo
+  
+###############################################################################
+
+do (App) ->
+  class QueueDemo
+          
+    constructor: (container) ->
+      _init.call this
+
+    # Private
+    
+    _init = ->
+      console.log this    
+    
+  App.QueueDemo = QueueDemo
+
+###############################################################################                  
+
 $ ->
+  stackDemo = new App.StackDemo "stack"
+  queueDemo = new App.QueueDemo "queue"
--- app/compiled/app.js
+++ app/compiled/app.js
@@ -1,3 +1,50 @@
 (function() {
-  $(function() {});
+  if (typeof App === "undefined" || App === null) {
+    App = {};
+  }
+  (function(App) {
+    var StackDemo;
+    StackDemo = (function() {
+      var _init;
+      function StackDemo(container, width, height) {
+        if (width == null) {
+          width = 640;
+        }
+        if (height == null) {
+          height = 480;
+        }
+        this._paper = Raphael(container, width, height);
+        this._width = width;
+        this._height = height;
+        _init.call(this);
+      }
+      _init = function() {
+        return this._paper.rect(0, 0, this._width, this._height, 10).attr({
+          fill: "gray",
+          stroke: "none"
+        });
+      };
+      return StackDemo;
+    })();
+    return App.StackDemo = StackDemo;
+  })(App);
+  (function(App) {
+    var QueueDemo;
+    QueueDemo = (function() {
+      var _init;
+      function QueueDemo(container) {
+        _init.call(this);
+      }
+      _init = function() {
+        return console.log(this);
+      };
+      return QueueDemo;
+    })();
+    return App.QueueDemo = QueueDemo;
+  })(App);
+  $(function() {
+    var queueDemo, stackDemo;
+    stackDemo = new App.StackDemo("stack");
+    return queueDemo = new App.QueueDemo("queue");
+  });
 }).call(this);
