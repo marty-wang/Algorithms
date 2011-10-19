5df0ad0afc0e06a6f46e487cd177c38fcf158d9f
Mo Wang <mo.hy.wang@gmail.com>
2011-10-19 11:44:53 -0700

    set up bst_app

---
+++ demo/app/coffee/bst_app.coffee
@@ -0,0 +1,25 @@
+do (App) ->
+
+    class BSTDemo
+    
+        constructor: (container, width = 640, height = 480) ->
+            @_width = width
+            @_height = height
+            @_paper = Raphael container, width, height
+
+            _setup.call this
+    
+    # Private
+
+    _setup = ->
+        @_paper.rect(0, 0, @_width, @_height, 10).attr({fill: "gray", stroke: "none"})
+    
+    # End of BSTDemo
+    App.BSTDemo = BSTDemo      
+
+###############################################################################
+
+$ ->
+    bstDemo = new App.BSTDemo "bst-demo"
+
+    console.log bstDemo
--- demo/app/compiled/bst_app.js
+++ demo/app/compiled/bst_app.js
@@ -1,3 +1,32 @@
 (function() {
-
+  (function(App) {
+    var BSTDemo, _setup;
+    BSTDemo = (function() {
+      function BSTDemo(container, width, height) {
+        if (width == null) {
+          width = 640;
+        }
+        if (height == null) {
+          height = 480;
+        }
+        this._width = width;
+        this._height = height;
+        this._paper = Raphael(container, width, height);
+        _setup.call(this);
+      }
+      return BSTDemo;
+    })();
+    _setup = function() {
+      return this._paper.rect(0, 0, this._width, this._height, 10).attr({
+        fill: "gray",
+        stroke: "none"
+      });
+    };
+    return App.BSTDemo = BSTDemo;
+  })(App);
+  $(function() {
+    var bstDemo;
+    bstDemo = new App.BSTDemo("bst-demo");
+    return console.log(bstDemo);
+  });
 }).call(this);
